b84192ce61de77f227f0784b2b23b0a86bc3802a
Schen0001 <chen.steven609@gmail.com>
2021-09-01 15:05:41 -0700

    adding forms and styling a bit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,46 @@
 class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:show]
 
-    
+    def welcome
+    end
+
+    def index
+        @posts = Post.all.order(created_at: :desc)
+    end
+
+    def show 
+        @post = Post.find(params[:id])
+    end
+
+    def new 
+        @new_post = Post.new
+    end
+
+    def create 
+        @new_post = Post.new params.require(:post).permit(:title, :body)
+        if @new_post.save 
+            redirect_to posts_path
+        else
+            render new_post_path
+        end
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update 
+        @post = Post.find(params[:id])
+        if @post.update params.require(:post).permit(:title, :body)
+            redirect_to post_path 
+        else
+            render edit_post_path
+        end
+    end
+
+    def destroy 
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,25 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resource :sessions, only: [:new, :destroy, :create]
 
-  
+  root 'posts#welcome'
+
+  # index
+  get '/posts' => 'posts#index', as: :posts
+
+  # new
+  get '/posts/new' => 'posts#new', as: :new_post
+  post '/posts' => 'posts#create'
+
+  # show
+  get '/posts/:id' => 'posts#show', as: :post 
+
+  # edit
+  get '/posts/:id/edit' => 'posts#edit', as: :edit_post
+
+  # patch
+  patch '/posts/:id' => 'posts#update'
+
+  # delete
+  delete '/posts/:id' => 'posts#destroy'
+
 end
